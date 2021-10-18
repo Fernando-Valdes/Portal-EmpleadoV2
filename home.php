31c4f05ec7fe7cf2af6c06df9d2f3537b8a595eb
Fernando-Valdes <55217268+Fernando-Valdes@users.noreply.github.com>
2021-10-18 14:46:16 -0500

    18/-10-21
    datos

---
+++ home.php
@@ -0,0 +1,99 @@
+<?php
+  include("head.php");
+  include("menu.php");
+?>
+  <!-- Titulo del Home -->
+  <div class="right_col" role="main">
+    <div class="">
+      <div class="page-title">
+          <h3>Tribunal Administrativo del Poder Judicial del Estado de Chiapas - Portal del Empleado</h3>
+        <div class="title_right">
+        </div>
+      </div>
+
+       <!-- Titulo del banner Anuncios del personal -->
+      <div class="clearfix"></div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="x_panel">
+            <div class="x_title">
+              <h2>Anuncios del Tribunal Administrativo al Personal</h2>
+              <ul class="nav navbar-right panel_toolbox">
+                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                </li>
+                <li class="dropdown">
+                </li>
+                <li><a class="close-link"><i class="fa fa-close"></i></a>
+                </li>
+              </ul>
+              <div class="clearfix"></div>
+            </div>
+            <div class="x_content">
+              <section class="content invoice">
+                <div class="row">
+                  <div class="  invoice-header">
+                    <h1>
+                        <i class="fa fa-globe"></i> Invoice.
+                          <small class="pull-right">Date: 16/08/2016</small>
+                      </h1>
+                  </div>
+                </div>
+
+                <!-- Table row -->
+                <div class="row">
+                  <div class="  table">
+                    <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <th>Qty</th>
+                          <th>Product</th>
+                          <th>Serial #</th>
+                          <th style="width: 59%">Description</th>
+                          <th>Subtotal</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td>Call of Duty</td>
+                          <td>455-981-221</td>
+                          <td>El snort testosterone trophy driving gloves handsome gerry Richardson helvetica tousled street art master testosterone trophy driving gloves handsome gerry Richardson
+                          </td>
+                          <td>$64.50</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Need for Speed IV</td>
+                          <td>247-925-726</td>
+                          <td>Wes Anderson umami biodiesel</td>
+                          <td>$50.00</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Monsters DVD</td>
+                          <td>735-845-642</td>
+                          <td>Terry Richardson helvetica tousled street art master, El snort testosterone trophy driving gloves handsome letterpress erry Richardson helvetica tousled</td>
+                          <td>$10.70</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Grown Ups Blue Ray</td>
+                          <td>422-568-642</td>
+                          <td>Tousled lomo letterpress erry Richardson helvetica tousled street art master helvetica tousled street art master, El snort testosterone</td>
+                          <td>$25.99</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php
+  include("footer.php");
+?>
